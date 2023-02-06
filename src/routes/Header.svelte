f30caea6efa007af92128f4ff2ee79dd92152b3a
Amelia Wattenberger <wattenberger@gmail.com>
2023-02-05 19:33:35 -0800

    remove unused images

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,5 @@
 <script>
-	import { page } from '$app/stores';
 	import { title } from '$/content/general.json';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header class="w-full px-10 py-6 font-sans">
